48b14d8ae11082e30f6018bebaf249c555b96544
john j <johnjjdev690@gmail.com>
2024-01-15 15:34:27 +0000

    git add .

--- my-defi-app/src/utils/ethers.js
+++ my-defi-app/src/utils/ethers.js
@@ -1,10 +1,17 @@
 import { ethers } from 'ethers';
 
-// Connect to the network
-let provider = ethers.getDefaultProvider('rinkeby'); // Use 'homestead' for mainnet
+// Connect to the Goerli test network
+let provider = ethers.getDefaultProvider('goerli');
 
-// Contract information
+// Your contract's information
 const contractAddress = '0xfd27d432e28d719cdc2ce5eaed0556669150eec4';
 const contractABI = require('../abi/AdvancedDeFiPayPal.json');
 
-export const advancedDeFiPayPalContract = new ethers.Contract(contractAddress, contractABI
+export const advancedDeFiPayPalContract = new ethers.Contract(contractAddress, contractABI, provider);
+
+// Function to get a signer for transactions
+export const getSigner = async () => {
+    await window.ethereum.request({ method: 'eth_requestAccounts' });
+    let userProvider = new ethers.providers.Web3Provider(window.ethereum);
+    return userProvider.getSigner();
+};
